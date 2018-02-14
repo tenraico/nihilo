fc99285233f1adfe8ff3112fa118e1962d4962e1
Nihilo Coin <35467587+nihilocoin@users.noreply.github.com>
2018-02-15 07:48:22 +1100

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -167,7 +167,9 @@ public:
 		checkpointData = (CCheckpointData) {
 			boost::assign::map_list_of
 			(  0, uint256S("0x00000c62a60c42507246e548c2876864b976c69b3ab1d78c7454b0380f186e26")),
-			(  1253, uint256S("0x6e254c6f4fb49b0b44cf7f885133b0202f39d2cad1caf6fae68cee00c0d12316")),
+			(  970, uint256S("0x0000000000163f47319439a16aea9e6717fc768613f0f4d9f0600b80e01fc9d3")),
+			(  1260, uint256S("0x00000000003d69ca8f90073a8392acb01dcbaf148bf82200853404e118d532f7")),
+			
 		        //1515715200,    // * UNIX timestamp of last checkpoint block
 			//30252,        // * total number of transactions between genesis and last checkpoint
 					   //   (the tx=... number in the SetBestChain debug.log lines)
